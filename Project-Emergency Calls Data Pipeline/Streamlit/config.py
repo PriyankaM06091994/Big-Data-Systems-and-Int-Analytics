5afbb9952a40299853d05744e1bbb95b3085c589
Keerti <ojha.ke@northeatern.edu>
2021-04-30 02:29:10 -0400

    Adding new code

--- Project-Emergency Calls Data Pipeline/Streamlit/config.py
+++ Project-Emergency Calls Data Pipeline/Streamlit/config.py
@@ -1,9 +1,11 @@
 aws = {
-    "secret_id": "",
-    "access_key": ""
+    "secret_id": "AKIAXZLTWQIDYOIAK4WU",
+    "access_key": "kD52+f3sNGZW0+X8U6/e8t2R9NpX4fYXzqBaqSIe",
+    "region": "us-east-1"
 }
 
 google_maps_api = {
     "key": "AIzaSyBWyJXeBhUSMuu9dB7YlMIXFlY0zlr37vg"
 }
 
+#aws_access_key_id =config.aws["secret_id"] ,aws_secret_access_key = config.aws["access_key"]
