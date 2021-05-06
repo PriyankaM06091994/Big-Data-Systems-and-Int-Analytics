87e7966ebb3b542b353bbf19f845d8a238b7d6e0
PriyankaM06091994 <59594174+PriyankaM06091994@users.noreply.github.com>
2021-05-06 12:49:44 -0400

    Update config.py

--- Project-Emergency Calls Data Pipeline/Streamlit/config.py
+++ Project-Emergency Calls Data Pipeline/Streamlit/config.py
@@ -1,11 +1,11 @@
 aws = {
-    "secret_id": "AKIAXZLTWQIDYOIAK4WU",
-    "access_key": "kD52+f3sNGZW0+X8U6/e8t2R9NpX4fYXzqBaqSIe",
+    "secret_id": "*************************",
+    "access_key": "***************************************",
     "region": "us-east-1"
 }
 
 google_maps_api = {
-    "key": "AIzaSyBWyJXeBhUSMuu9dB7YlMIXFlY0zlr37vg"
+    "key": "***********************************"
 }
 
 #aws_access_key_id =config.aws["secret_id"] ,aws_secret_access_key = config.aws["access_key"]
