3c7b25fb794e31b5db60fbb022d218c9d35583f6
Tanvi Gurav <59846364+guravtanvi@users.noreply.github.com>
2021-05-15 00:53:19 -0400

    Update base.py

--- Assignment 1/Snowflake/Sql Alchemy/base.py
+++ Assignment 1/Snowflake/Sql Alchemy/base.py
@@ -5,7 +5,7 @@ from sqlalchemy.ext.declarative import declarative_base
 from sqlalchemy.orm import sessionmaker
 #import snowflake.connector as sf
 
-engine = create_engine("snowflake://malpekarpriyanka6:Manisha!1994@tza57982.us-east-1/sevir")
+engine = create_engine("snowflake://username:password@account/sevir")
 
 engine = create_engine("postgresql://postgres:postgres@localhost:5432/postgres")
 
